16365d2cd0affd1fe843542c16009b1a20650d9b
Charithdabare <dabarecharith@gmail.com>
2020-03-20 15:09:59 +0530

    Login,signup and profile management

---
+++ app/src/main/java/com/example/services/passoword.kt
@@ -0,0 +1,12 @@
+package com.example.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class passoword : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.password)
+    }
+}
