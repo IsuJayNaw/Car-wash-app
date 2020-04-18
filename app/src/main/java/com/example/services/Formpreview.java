95bab669fd06501fd45f43bd6e3f887660f5b75d
IsuJay <isujayawardana1220@gmail.com>
2020-04-18 11:07:32 +0530

    My service part is over

--- app/src/main/java/com/example/services/Formpreview.java
+++ app/src/main/java/com/example/services/Formpreview.java
@@ -2,13 +2,54 @@ package com.example.services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Formpreview extends AppCompatActivity {
+    DatabaseHelper myDb;
+    Button btnAddData;
+    EditText userMobile, userCarNo, userCarModel, userService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formpreview);
+
+
+        myDb = new DatabaseHelper(this);
+        userMobile = findViewById(R.id.userMobile);
+        userCarNo = findViewById(R.id.userCarNo);
+        userCarModel = findViewById(R.id.userCarModel);
+        userService = findViewById(R.id.userService);
+        btnAddData = findViewById(R.id.btnAddData);
+        AddData();
+    }
+
+
+    public void AddData(){
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = myDb.insertData(userMobile.getText().toString(),userCarNo.getText().toString(),userCarModel.getText().toString(),userService.getText().toString());
+
+                if(isInserted == true ){
+                    Toast.makeText(Formpreview.this, "DaTa Inserted", Toast.LENGTH_LONG).show();
+                    //Toasty.success(Formpreview.this, "Data Insert!", Toast.LENGTH_LONG).show();
+                }else {
+                   // Toasty.error(Formpreview.this, "Data Insert!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(Formpreview.this, "Data Insert!", Toast.LENGTH_LONG).show();
+                }
+
+                Intent intent =new Intent(getApplicationContext(),Options.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/services/Options.java
+++ app/src/main/java/com/example/services/Options.java
@@ -1,22 +1,93 @@
 package com.example.services;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class Options extends AppCompatActivity {
-    Intent button;
+    Button btnAdd,btnview,btnEdit,btnDelete,btnBook;
+    DatabaseHelper  myviewDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        myviewDb = new DatabaseHelper(this);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnview = findViewById(R.id.btnDataView);
+        btnEdit= findViewById(R.id.btnEdit);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnBook = findViewById(R.id.btnBook);
+        viewAll();
+
+
+        //Add Service
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Formpreview.class);
+                startActivity(intent);
+            }
+        });
+
+        //Delete Section
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),DeleteService.class);
+                startActivity(intent);
+            }
+        });
+
+        //Edit Service
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),EditServiceDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
-    public void formPre(View view) {
-        Intent i = new Intent(this,Formpreview.class);
-        startActivity(i);
+    public void viewAll(){
+        btnview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res = myviewDb.getAllData();
+                if (res.getCount() <= 0){
+                    //Show Message
+                    ShowMessages("Error", "Nothing Found");
+                }else {
+                    StringBuffer buffer = new StringBuffer();
+                    while (res.moveToNext()) {
+                        buffer.append("ID : " + res.getString(0) + "\n");
+                        buffer.append("Mobile :" + res.getString(1) + "\n");
+                        buffer.append("Car No :" + res.getString(2) + "\n");
+                        buffer.append("Car Model :" + res.getString(3) + "\n");
+                        buffer.append("Service : " + res.getString(4) + "\n\n");
+
+                    }
+                    //Show all data
+                    ShowMessages("Data", buffer.toString());
+                }
+            }
+        });
+    }
+
+    public  void ShowMessages(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 }
