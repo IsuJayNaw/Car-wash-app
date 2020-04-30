b07f0da5457ff81679dff5fdac3f9a14d1eea9cd
pramodC135 <pramodmadhushan4@gmail.com>
2020-04-30 21:54:02 +0530

    Payment

--- app/src/main/java/com/example/services/Pay.java
+++ app/src/main/java/com/example/services/Pay.java
@@ -2,13 +2,26 @@ package com.example.services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pay extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pay);
+        setContentView(R.layout.activity_main);
+
+        Button pay= (Button) findViewById(R.id.chariya);
+
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editIntent = new Intent(MainActivity.this, Payment.class);
+                startActivity(editIntent);
+            }
+        });
     }
 }
